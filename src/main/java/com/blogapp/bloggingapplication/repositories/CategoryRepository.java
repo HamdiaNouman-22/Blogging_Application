72025c92f58b36af7a09d0cba81bee932dcaf0db
Hamdia Nouman <noumanhamdia@gmail.com>
2024-09-28 12:12:50 +0500

    Remove JPA repositories from Git tracking

--- src/main/java/com/blogapp/bloggingapplication/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blogapp.bloggingapplication.repositories;
-
-import com.blogapp.bloggingapplication.entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-}
--- src/main/java/com/blogapp/bloggingapplication/repositories/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blogapp.bloggingapplication.repositories;
-
-import com.blogapp.bloggingapplication.entities.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Integer> {
-}
