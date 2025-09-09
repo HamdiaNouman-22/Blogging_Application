97004aa3d72d330f630e7d63becd6e0a82ea52f3
Hamdia Nouman <noumanhamdia@gmail.com>
2025-09-09 14:18:21 +0500

    Added Swagger documentation

---
+++ src/main/java/com/blogapp/bloggingapplication/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.blogapp.bloggingapplication;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Blogging Application API")
+                        .version("1.0")
+                        .description("API documentation for Blogging Application backend"));
+    }
+}
